93c2427c6b7a54c66f3315f2683e64ccfdaee713
leombpunk <leomb_punk@hotmail.com>
2024-02-26 17:16:32 -0300

    agregado datos y cursos al segmento educación

--- src/components/education.jsx
+++ src/components/education.jsx
@@ -1,4 +1,9 @@
 import { Disclosure, Transition } from "@headlessui/react"
+import {
+  CheckBadgeIcon,
+  ChevronDoubleDownIcon,
+} from "@heroicons/react/24/outline"
+import educationData from "../data/educationData"
 
 const Education = () => {
   return (
@@ -9,70 +14,107 @@ const Education = () => {
       >
         <div className='w-full h-full px-5 lg:px-10 py-5 text-balance flex flex-col gap-3 border-l-8 border-sky-300/50 bg-blue-900/45 rounded-md shadow-lg order-2 lg:order-1'>
           <ol>
-            <li className='border-l-2 border-sky-300/50'>
-              <div className='flex'>
-                <div className='bg-sky-300 w-6 h-6 flex items-center justify-center rounded-full -ml-3.5'></div>
-                <div className='block p-6 rounded-lg shadow-lg bg-blue-950 border border-sky-300/50 max-w-[19rem] md:max-w-md lg:max-w-[27rem] xl:max-w-xl ml-6 mb-10'>
-                  <div className='flex justify-between mb-4'>
-                    <h3 className='font-medium text-white duration-500 transition ease-in-out text-sm'>
-                      New Web Design
-                    </h3>
-                    <h3 className='font-medium text-white duration-500 transition ease-in-out text-sm'>
-                      04 / 02 / 2022
-                    </h3>
-                  </div>
-                  <p className='text-gray-300 mb-6'>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    Quisque scelerisque diam non nisi semper, et elementum lorem
-                    ornare. Maecenas placerat facilisis mollis. Duis sagittis
-                    ligula in sodales vehicula.
-                  </p>
-                </div>
-              </div>
-            </li>
-            <li className='border-l-2 border-sky-300/50'>
-              <div className='flex'>
-                <div className='bg-sky-300 w-6 h-6 flex items-center justify-center rounded-full -ml-3.5'></div>
-                <div className='block p-6 rounded-lg shadow-lg bg-blue-950 border border-sky-300/50 max-w-[19rem] md:max-w-md lg:max-w-[27rem] xl:max-w-xl ml-6 mb-10'>
-                  <div className='flex justify-between mb-4'>
-                    <h3 className='font-medium text-white duration-500 transition ease-in-out text-sm'>
-                      21 000 Job Seekers
-                    </h3>
-                    <h3 className='font-medium text-white duration-500 transition ease-in-out text-sm'>
-                      12 / 01 / 2022
-                    </h3>
-                  </div>
-                  <p className='text-gray-300 mb-6'>
-                    Libero expedita explicabo eius fugiat quia aspernatur autem
-                    laudantium error architecto recusandae natus sapiente sit
-                    nam eaque, consectetur porro molestiae ipsam an deleniti.
-                  </p>
-                </div>
-              </div>
-            </li>
-            <li className='border-l-2 border-sky-300/50'>
-              <div className='flex'>
-                <div className='bg-sky-300 w-6 h-6 flex items-center justify-center rounded-full -ml-3.5'></div>
-                <div className='block p-6 rounded-lg shadow-lg bg-blue-950 border border-sky-300/50 max-w-[19rem] md:max-w-md lg:max-w-[27rem] xl:max-w-xl ml-6 mb-10'>
-                  <div className='flex justify-between mb-4'>
-                    <h3 className='font-medium text-white duration-500 transition ease-in-out text-sm'>
-                      Awesome Employers
-                    </h3>
-                    <h3 className='font-medium text-white duration-500 transition ease-in-out text-sm'>
-                      21 / 12 / 2021
-                    </h3>
-                  </div>
-                  <p className='text-gray-300 mb-6'>
-                    Voluptatibus temporibus esse illum eum aspernatur, fugiat
-                    suscipit natus! Eum corporis illum nihil officiis tempore.
-                    Excepturi illo natus libero sit doloremque, laborum
-                    molestias rerum pariatur quam ipsam necessitatibus incidunt,
-                    explicabo.
-                  </p>
-                </div>
-              </div>
-            </li>
+            {educationData
+              ? educationData.map((studies) =>
+                  studies?.category === "studies"
+                    ? studies.data.map((data, index) => (
+                        <li
+                          className='border-l-2 border-sky-300/50'
+                          key={index}
+                        >
+                          <div className='flex'>
+                            <div className='bg-sky-300 w-6 h-6 flex items-center justify-center rounded-full -ml-3.5'></div>
+                            <div className='block p-6 rounded-lg shadow-lg bg-blue-950 border border-sky-300/50 w-full max-w-[19rem] md:max-w-md lg:max-w-[27rem] xl:max-w-xl ml-6 mb-10'>
+                              <div className='flex justify-between mb-2'>
+                                <h3 className='font-medium text-white duration-500 transition ease-in-out text-xl'>
+                                  {data.name}
+                                </h3>
+                                <h3 className='font-medium text-white duration-500 transition ease-in-out text-xl'>
+                                  <CheckBadgeIcon
+                                    width={28}
+                                    height={28}
+                                    className='text-sky-300'
+                                  />
+                                </h3>
+                              </div>
+                              <p className='text-gray-300 mb-1'>
+                                {data.institute}
+                              </p>
+                              <p className='text-gray-300 mb-1'>
+                                {`Inicio: ${data.start} - Fin: ${data.end}`}
+                              </p>
+                              <p className='text-gray-300 mb-1'>{data.place}</p>
+                            </div>
+                          </div>
+                        </li>
+                      ))
+                    : ""
+                )
+              : ""}
           </ol>
+
+          <Disclosure as='div' className='w-full'>
+            <Disclosure.Button className='w-full flex flex-row items-center justify-center leading-6 gap-3 text-2xl font-semibold duration-500 rounded-xl p-3 mb-5 text-white hover:text-sky-300 border border-transparent hover:shadow-md hover:shadow-sky-300 hover:border-sky-300'>
+              Cursos
+              <ChevronDoubleDownIcon
+                width={22}
+                height={22}
+                className='flex-none animate-bounce'
+                aria-hidden='true'
+              />
+            </Disclosure.Button>
+            <Transition
+              enter="transition ease-out duration-500"
+              enterFrom="opacity-0 translate-y-1"
+              enterTo="opacity-100 translate-y-0"
+              leave="transition ease-in duration-500"
+              leaveFrom="opacity-100 translate-y-0"
+              leaveTo="opacity-0 translate-y-1">
+              <Disclosure.Panel>
+                <ol>
+                  {educationData
+                    ? educationData.map((studies) =>
+                        studies?.category === "courses"
+                          ? studies.data.map((data, index) => (
+                              <li
+                                className='border-l-2 border-sky-300/50'
+                                key={index}
+                              >
+                                <div className='flex'>
+                                  <div className='bg-sky-300 w-6 h-6 flex items-center justify-center rounded-full -ml-3.5'></div>
+                                  <div className='block p-6 rounded-lg shadow-lg bg-blue-950 border border-sky-300/50 w-full max-w-[19rem] md:max-w-md lg:max-w-[27rem] xl:max-w-xl ml-6 mb-10'>
+                                    <div className='flex justify-between mb-2'>
+                                      <h3 className='font-medium text-white duration-500 transition ease-in-out text-xl'>
+                                        {data.name}
+                                      </h3>
+                                      <h3 className='font-medium text-white duration-500 transition ease-in-out text-xl'>
+                                        <CheckBadgeIcon
+                                          width={28}
+                                          height={28}
+                                          className='text-sky-300'
+                                        />
+                                      </h3>
+                                    </div>
+                                    <p className='text-gray-300 mb-1'>
+                                      {data.institute}
+                                    </p>
+                                    <p className='text-gray-300 mb-1'>
+                                      {`Inicio: ${data.start} - Fin: ${data.end}`}
+                                    </p>
+                                    <p className='text-gray-300 mb-1'>
+                                      {`Duración: ${data.duration}`}
+                                    </p>
+                                  </div>
+                                </div>
+                              </li>
+                            ))
+                          : ""
+                      )
+                    : ""}
+                </ol>
+              </Disclosure.Panel>
+            </Transition>
+          </Disclosure>
         </div>
         <h2 className='text-4xl text-center lg:text-left font-bold text-balance h-full w-full lg:w-96 px-5 py-5 order-1 lg:order-2'>
           Educación
--- src/data/educationData.js
+++ src/data/educationData.js
@@ -1,16 +1,62 @@
 const educationData = [
   {
-    name: '',
-    institute: '',
-    start: '',
-    end: '',
-    place: '',
+    category: "studies",
+    data: [
+      {
+        name: "Analista de Sistemas de Computación",
+        institute: "Esc. Superior Normal 10 - “Anexo San Antonio”",
+        start: "MARZO 2014",
+        end: "DICIEMBRE 2023",
+        place: "Posadas-Misiones-Argentina",
+      },
+      {
+        name: "Analista Programador",
+        institute: "Esc. Superior Normal 10 - “Anexo San Antonio”",
+        start: "MARZO 2014",
+        end: "MARZO 2021",
+        place: "Posadas-Misiones-Argentina",
+      },
+      {
+        name: "Bachillerato Técnico Contable",
+        institute: "Instituto Privado Nueva Era",
+        start: "FEBRERO 2007",
+        end: "DICIEMBRE 2009",
+        place: "San Lorenzo-Paraguay",
+      },
+    ],
   },
   {
-
-  },
-  {
-    
+    category: "courses",
+    data: [
+      {
+        name: "Argentina Programa #YoProgramo",
+        institute: "Ministerio de Desarrollo Productivo - CESSI Argentina - INTI",
+        start: "06/2022",
+        end: " 01/2023",
+        duration: "7 meses",
+      },
+      {
+        name: "Argentina Programa #SéProgramar",
+        institute: "Ministerio de Desarrollo Productivo - CESSI Argentina - INTI",
+        start: "08/2021",
+        end: "10/2021",
+        duration: "3 meses",
+      },
+      {
+        name: "Informática, Operador de Computadoras",
+        institute: "Escuela para Adultos nº31 de Educación",
+        start: "03/2013",
+        end: "12/2013",
+        duration: "12 meses",
+      },
+      {
+        name: "Taller Especialista en Reparación, Armado y Mantenimiento de Computadoras",
+        institute: "Paseo Cultural “La Terminal”",
+        start: "03/2014",
+        end: "03/2015",
+        duration: "12 meses",
+      },
+    ],
   },
 ]
 export default educationData
