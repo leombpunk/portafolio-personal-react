1d857b94f4647b0b2889e98b9a44bddfc7f24453
leombpunk <leomb_punk@hotmail.com>
2024-02-28 15:00:04 -0300

    instalado flowbite para un carousel xd

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}","node_modules/flowbite-react/lib/esm/**/*.js"],
   theme: {
     extend: {
       screens: {
@@ -8,5 +8,7 @@ export default {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin')
+  ],
 }
